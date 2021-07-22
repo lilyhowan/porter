7c27e52651f3b06aa26d1282111c58e31c4b82af
Lily Howan <lily.howan@gmail.com>
2021-07-23 02:39:14 +1200

    Added error handling to birthday command

--- cogs/acnh.py
+++ cogs/acnh.py
@@ -8,17 +8,20 @@ class ACNH(commands.Cog):
 
     @commands.command(name="birthday", help="Returns specified villager's birthday")
     async def birthday(self, ctx, villager):
-        response = urllib.request.urlopen(f"http://acnhapi.com/v1/villagers/")
-        birthday_data = json.loads(response.read())
-        villager = villager.capitalize()
-
-        for i in birthday_data:
-            if villager in birthday_data[i]["name"].values():
-                villager_id = birthday_data[i]["id"]
-        
-        response = urllib.request.urlopen(f"http://acnhapi.com/v1/villagers/{villager_id}")
-        villager_data = json.loads(response.read())
-        await ctx.send(f"{villager_data['name']['name-USen']}'s birthday is on {villager_data['birthday-string']} ({villager_data['birthday']}).")
+        try:
+            response = urllib.request.urlopen(f"http://acnhapi.com/v1/villagers/")
+            birthday_data = json.loads(response.read())
+            villager = villager.capitalize()
+            # find villager ID from given villager name
+            for i in birthday_data:
+                if villager in birthday_data[i]["name"].values():
+                    villager_id = birthday_data[i]["id"]
+                    break
+            response = urllib.request.urlopen(f"http://acnhapi.com/v1/villagers/{villager_id}")
+            villager_data = json.loads(response.read())
+            await ctx.send(f"{villager_data['name']['name-USen']}'s birthday is on {villager_data['birthday-string']} ({villager_data['birthday']}).")
+        except:
+            await ctx.send("Specified villager could not be found.")
 
 def setup(bot):
     bot.add_cog(ACNH(bot))
